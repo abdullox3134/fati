9a1af2ad319f6e732cc8b9b04978be87b838a943
abdullox <abdulloxislomov39@gmail.com>
2024-06-05 18:09:43 +0500

    jazzmin

--- config/settings.py
+++ config/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -190,3 +191,77 @@ CKEDITOR_CONFIGS = {
     },
 }
 
+JAZZMIN_SETTINGS = {
+    # "site_title": "Foydali Havolalar",
+    "site_brand": "Fanlar Akademiyasi",
+    # "site_logo": "jadid.jpg",
+    "welcome_sign": "Xush Kelibsiz",
+    "copyright": "Fati",
+    # "search_model": ["app.model_name"],
+
+    "topmenu_links": [
+        {"model": "auth.User"},
+        # {"model": "auth.Group"},
+
+        # {"name": "Foydali Havolalar", "url": "foydali_havolalar"},
+        # {"name": "Hikmatli Sozlar", "url": "hikmatli_sozlar"},
+        # {"name": "Hujjatlar", "url": "hujjatlar"},
+        # {"name": "Ishtirokchilar", "url": "ishtirokchilar"},
+        # {"name": "Jadidlar", "url": "jadidlar"},
+        # {"name": "Manbalar", "url": "manbalar"},
+        # {"name": "Sahifalar", "url": "sahifalar"},
+        # {"name": "Slayder", "url": "slayder"},
+        # {"name": "Tadbirlar", "url": "tadbirlar"},
+        # {"name": "Tanlovlar", "url": "tanlovlar"},
+    ],
+
+    "navigation_expanded": False,
+
+    "hide_apps": ['auth'],
+
+    "show_ui_builder": True,
+
+    "changeform_format": "collapsible",
+
+    # "usermenu_links": [
+    # {"name": "Profile", "url": "profile"},
+    # {"name": "Logout", "url": "logout"},
+    # {"name": "Login", "url": "login"},
+    # {"name": "Signup", "url": "signup"},
+
+    #     ]
+
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": False,
+    "accent": "accent-primary",
+    "navbar": "navbar-white navbar-light",
+    "no_navbar_border": False,
+    "navbar_fixed": True,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": False,
+    "sidebar": "sidebar-dark-primary",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": True,
+    "theme": "default",
+    "dark_mode_theme": None,
+    "button_classes": {
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    },
+    "actions_sticky_top": False
+}
